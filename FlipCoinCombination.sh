192b934664c1cf786c552814ee9f8c24ed40247a
Shreya Malviya <malviyashreya26@gmail.com>
2021-05-01 21:08:08 +0530

    As a simulator, loop to create a Doublet Combination. Store the doublet combination as well as the percentage.

--- FlipCoinCombination.sh
+++ FlipCoinCombination.sh
@@ -1,10 +1,37 @@
-#!/bin/bash
+!/bin/bash
 echo "WELCOME TO FLIP COIN COMBINATION"
 
-a=$(( RANDOM%2 ));
-if [ $a == 1 ];
-then
-        echo "Heads";
-else
-        echo "Tails";
-fi
+
+head=0;
+tail=0;
+read -p "Enter number of flip required " n
+for (( i=0; i<n; i++ ))
+do
+	a=$(( RANDOM%2 ));
+	if [ $a == 1 ];
+	then
+         	head=$(($head+1))
+	else
+        	tail=$(($tail+1))
+	fi
+	d=$(( RANDOM%4 ));
+	case $d in
+		0) hh=$(($hh+1))
+			;;
+		1) ht=$(($ht+1))
+			;;
+		2) th=$(($th+1))
+			;;
+		3) tt=$(($tt+1))
+			;;
+		*) echo "Went  wrong"
+			;;
+	esac
+done
+
+echo "Number of head win" $head
+echo "Number of tail win" $tail
+echo "Number of hh combination " $hh
+echo "Number of ht combination " $ht
+echo "Number of th combination " $th
+echo "Number of tt combination " $tt
